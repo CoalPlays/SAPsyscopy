b4b66e3e3955da8f38618b6ab5a479e6cbcac126
CoalPlays <coalcrafter202@gmail.com>
2020-12-03 09:17:30 +0100

    Added all Files

---
+++ javaCode/src/jco/SYScopy.java
@@ -0,0 +1,12 @@
+package jco;
+
+import com.sap.conn.jco.AbapException;
+import com.sap.conn.jco.JCoDestination;
+import com.sap.conn.jco.JCoDestinationManager;
+import com.sap.conn.jco.JCoException;
+import com.sap.conn.jco.JCoFunction;
+import com.sap.conn.jco.JCoStructure;
+
+public class SYScopy {
+    
+}
